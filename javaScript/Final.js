366b0e4c7d4f60a1bbecc3ad20bdd4c258f716d1
Samueljosephj <113227348+Samueljosephj@users.noreply.github.com>
2022-10-15 11:10:31 +0530

    CSS,images and Js included

---
+++ javaScript/Final.js
@@ -0,0 +1,36 @@
+// var mainimg = document.getElementById("mainimg");
+// var smallimg = document.getElementsByClassName("small-img");
+//
+// smallimg[0].onclick = function () {
+//   mainimg.src = smallimg[0].src;
+// }
+//
+//
+// smallimg[1].onclick = function () {
+//   mainimg.src = smallimg[1].src;
+// }
+//
+//
+// smallimg[2].onclick = function () {
+//   mainimg.src = smallimg[2].src;
+// }
+//
+//
+// smallimg[3].onclick = function () {
+//   mainimg.src = smallimg[3].src;
+// }
+//
+//
+// // add cart
+//
+// var addToCartbutton =document.getElementsByClassName('addcart');
+// var price = document.querySelector(".cost").innerHTML;
+//
+// {
+// addToCartbutton[0].onclick = function(){
+// document.querySelector('.addcart').innerHTML = "Added";
+//  localStorage.setItem("price",price);
+//
+//
+// }
+// }
---
+++ javaScript/index.js
@@ -0,0 +1,9 @@
+var numberOfCounts = document.querySelectorAll(".drum").length;
+//
+// for (var i = 0; i < numberOfCounts; i++ ){
+
+  document.querySelectorAll(".drum")[2].addEventListener("click", function ()  {
+    alert("i got clicked");
+  });
+
+}
